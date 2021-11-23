4ac9723239acd4a58e9cb25186cf68d2ab74c970
Francis Rousseau <frr@mybingli.com>
2021-11-23 19:16:01 +0100

    add default layout and material library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rarity-calculator';
+
+  allBackgrounds = ['Gold', 'Light Blue', 'Matchstick', 'Mint', 'Neon Green', 'Neon Pink', 'Orange', 'Parma', 'Pink', 'Red', 'Silver', 'Teal']
+  allSkins = ['Brown Skin', 'Dark Skin', 'Dark Tan', 'Sunbed Tan', 'Tan', 'White Skin', 'Zombie Skin']
+  allClothes = ['Clothes	Black Hoody', ' Black Vest & Blue Jeans', 'Clothes	Black Suit', 'Black Suit with Red Rose',
+    'Black Suit with White Rose', 'Black Vest & Blue Jeans', 'Blue Hawaiian Shirt & Blue Jeans', 'Brown Suit',
+    'Brown Suit', 'Green Suit', 'Pink Suit', 'Red Suit', 'Red T-Shirt & Blue Jeans', 'White Suit with Red Rose',
+    'White Suit with White Rose', 'White Suit', 'White T Shirt & Blue Jeans']
+  allFacialHair = ['Full Beard', 'Goatee', 'Handlebars', 'Moustache', 'No Facial Hair', 'None']
+  allHair = ['Black Hair', 'Blonde Hair', 'Dreadlocks', 'White Beard and Hair', 'Yellow Hair']
+  allHeadWear = ['Cap', 'Cowboy', 'Crown', 'Fedora white', 'Fedora', 'Halo', 'Horns', 'None', 'Pirate Hat', 'Sweatband']
+  allGlasses = ['3D Glasses', 'Anime Glasses', 'Black Glasses', 'Black Sunglasses', 'Eye Glasses', 'Gold Glasses', 'Green Sunglasses',
+    'Normal Eye', 'Red Shades', 'Sleep Mask', 'Square Glasses']
+  allWeapons = ['none', 'Gold Hand Gun', 'Gold Machine Gun', 'Rifle', 'Hand Gun', 'Machine Gun']
+  allAccessories = ['none', 'Cigarette', 'Cuban Cigar', 'Face Mask', 'Gold Bitcoin Chain', 'Gold Chain with Ruby', 'Gold Ethereum Chain',
+    'Gold Watch', 'Red Neck Tie', 'Silver Bitcoin Chain', 'Silver Ethereum Chain', 'White Sneakers']
+
+
+  logo = 'https://eightbitmafia.com/assets/img/MafiaLogo.png'
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatOptionModule} from "@angular/material/core";
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
